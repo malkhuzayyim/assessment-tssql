7279d7ea7a28d3dbfcba925ba84911f0e9daa529
Ali Algmaty <alihewaigh@gmail.com>
2024-05-17 22:34:32 +0200

    implement update plan method

--- src/modules/plans/router.ts
+++ src/modules/plans/router.ts
@@ -34,4 +34,30 @@ export const plans = router({
         throw error;
       }
     }),
+  update: protectedProcedure
+    .input(
+      z.object({
+        id: z.number(),
+        name: z.string(),
+        price: z.number(),
+      })
+    )
+    .mutation(async ({ ctx: { user }, input }) => {
+      const { userId } = user;
+
+      try {
+        const userInDb = await db.query.users.findFirst({
+          where: eq(schema.users.id, userId),
+        });
+        if (!userInDb!.isAdmin) {
+          throw new trpcError({
+            code: "UNAUTHORIZED",
+          });
+        }
+        db.update(schema.plans).set(input).where(eq(schema.plans.id, input.id));
+        return { success: true };
+      } catch (error) {
+        throw error;
+      }
+    }),
 });
