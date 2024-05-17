016c75663b98a6df775a463cad9e2ba2c031427d
Ali Algmaty <alihewaigh@gmail.com>
2024-05-18 00:53:46 +0200

    Implemented upgrade price calculation function helper

---
+++ src/modules/plans/model.ts
@@ -0,0 +1,27 @@
+export const upgradePriceCalculation = ({
+  currentPrice,
+  newPrice,
+  daysRemaining,
+}: {
+  currentPrice: number;
+  newPrice: number;
+  daysRemaining: number;
+}) => {
+  if (daysRemaining < 0) {
+    throw new Error("Days remaining cannot be negative.");
+  }
+
+  if (newPrice <= currentPrice) {
+    throw new Error(
+      "New plan should be more expensive than current plan for upgrade"
+    );
+  }
+
+  const dailyCostCurrentPlan = currentPrice / 30;
+ 
+  const remainingCostCurrentCycle = dailyCostCurrentPlan * daysRemaining;
+
+  const upgradeCost = newPrice - remainingCostCurrentCycle;
+
+  return upgradeCost;
+};
